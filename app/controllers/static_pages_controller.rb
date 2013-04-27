c72838e5609d206ff7de5774cc051fed263643c1
Steve Sikoryak <stevein3d@gmail.com>
2013-04-27 15:54:59 -0400

    Add user form to home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,26 @@
 class StaticPagesController < ApplicationController
   def welcome
   	@math = 10*10
+  	@last_user_name = User.last.name
+  	@last_user_email = User.last.email
+
+  	@new_user = User.new
   end
+
+  def create
+  	puts "*************"
+  	puts params
+  	puts "*************"
+
+  	@new_user = User.new(params[:user])
+  	if @new_user.save
+  		flash[:success] = "Welcome home young one.  Welcome"
+  		redirect_to root_url
+  	else
+  		flash[:error] = "This failed, so sorry."
+  		redirect_to root_url
+  	end
+  end
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 CodeAlong::Application.routes.draw do
   root :to => "static_pages#welcome"
+  #get "/" => "static_pages#welcome"
+
+  post "create" => "static_pages#create"
 end
